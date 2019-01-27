0db7d1358837eaf9cfeb4d8213781752bb9372c7
V Shostak <shostak.vu@gmail.com>
2019-01-27 21:08:53 +0200

    added persistence config for Hibernate

---
+++ src/main/java/myproject/cviewer/config/PersistenceConfig.java
@@ -0,0 +1,57 @@
+package myproject.cviewer.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceConfig {
+
+    @Bean(name = "sessionFactory")
+    public SessionFactory sessionFactory() throws IOException {
+        final LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setHibernateProperties(hibernateProperties());
+        factory.setPackagesToScan("myproject.cviewer.dto");
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    @Bean(name = "dataSource", destroyMethod = "shutdown")
+	public DataSource dataSource() {
+ 		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder
+			.setType(EmbeddedDatabaseType.H2)
+			.addScript("schema.sql")
+			.addScript("test-data.sql")
+			.build();
+		return db;
+	}
+
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager transactionManager() throws IOException {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+
+    private static Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        hibernateProperties.put("hibernate.max_fetch_depth", 3);
+        hibernateProperties.put("hibernate.jdbc.fetch_size", 50);
+        hibernateProperties.put("hibernate.jdbc.batch_size", 10);
+        hibernateProperties.put("hibernate.show_sql", true);        
+        return hibernateProperties;
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS client;
+CREATE TABLE client (
+  id int NOT NULL auto_increment,
+  firstName varchar(50) NOT NULL,
+  lastName varchar(50) NOT NULL,
+  phoneNumber varchar(12) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(50),
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS admin;
+CREATE TABLE admin (
+  id int NOT NULL auto_increment,
+  firstName varchar(50) NOT NULL,
+  lastName varchar(50) NOT NULL,
+  phoneNumber varchar(12) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(50),
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS clientBlackList;
+CREATE TABLE clientBlackList (
+  id int NOT NULL auto_increment,
+  client_id int NOT NULL REFERENCES client(id) ON DELETE CASCADE,
+  description varchar(255) NOT NULL,
+  admin_id int NOT NULL REFERENCES admin(id) , 
+  PRIMARY KEY (id)
+);
